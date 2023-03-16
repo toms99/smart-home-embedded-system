280db3267d54ca62e0a672c57af3970a8affca95
toms99 <tomas.seguram@gmail.com>
2023-03-16 15:08:03 -0600

    adding lights and bulbs to the dashboard

---
+++ web-app/smart-home-web-app/src/app/classes/dashboard.ts
@@ -0,0 +1,18 @@
+export class dashboard {
+  door_TopLeft: boolean = false;
+  door_BottomLeft: boolean = false;
+  door_TopMiddle: boolean = false;
+  door_BottomMiddle: boolean = false;
+  door_Right: boolean = false;
+
+  light_TopLeft: boolean = false;
+  light_BottomLeft: boolean = false;
+  light_TopMiddle: boolean = false;
+  light_BottomMiddle: boolean = false;
+  light_Right: boolean = false;
+
+  all_doors: boolean = false;
+  all_lights: boolean = false;
+
+  cam_shoot: boolean = true;
+}
--- web-app/smart-home-web-app/src/app/dashboard/dashboard.component.ts
+++ web-app/smart-home-web-app/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { dashboard } from '../classes/dashboard';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,10 +13,49 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  MyVar = false
+  dashboard = new dashboard();
+  
+  
 
-  clickDoor() {
-    this.MyVar = !this.MyVar
+  clickDoor_TopLeft() {
+
+    this.dashboard.door_TopLeft = !this.dashboard.door_TopLeft
+  }
+
+  clickDoor_BottomLeft() {
+    this.dashboard.door_BottomLeft = !this.dashboard.door_BottomLeft
+  }
+
+  clickDoor_TopMiddle() {
+    this.dashboard.door_TopMiddle = !this.dashboard.door_TopMiddle
+  }
+
+  clickDoor_BottomMiddle() {
+    this.dashboard.door_BottomMiddle = !this.dashboard.door_BottomMiddle
+  }
+
+  clickDoor_Right() {
+    this.dashboard.door_Right = !this.dashboard.door_Right
+  }
+
+  turnLight_TopLeft() {
+    this.dashboard.light_TopLeft = !this.dashboard.light_TopLeft
+  }
+
+  turnLight_BottomLeft() {
+    this.dashboard.light_BottomLeft = !this.dashboard.light_BottomLeft
+  }
+
+  turnLight_TopMiddle() {
+    this.dashboard.light_TopMiddle = !this.dashboard.light_TopMiddle
+  }
+
+  turnLight_BottomMiddle() {
+    this.dashboard.light_BottomMiddle = !this.dashboard.light_BottomMiddle
+  }
+
+  turnLight_Right() {
+    this.dashboard.light_Right = !this.dashboard.light_Right
   }
 
 }
